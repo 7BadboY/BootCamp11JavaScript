56746e92ce97aeedf3326a675fcde2e11029ca38
Yarotskiy Sergey <abrawa@gmail.com>
2019-04-16 12:32:55 +0300

    working on m6

---
+++ js_m6/js/script.js
@@ -0,0 +1,42 @@
+'use strict';
+// =============================================Prototypes practice===========================
+// const obj = {};
+// const x = new Object();
+
+// console.log(obj);
+// console.log(x);
+
+
+// const parent = {
+//     parentProp: 'this is a parent prop',
+//     a: 5,
+// };
+
+// // const child = {
+// //     childProp: 'this is a child prop'
+// // };
+
+// // child.__proto__ = parent;
+// // console.log(child.parentProp);
+
+// const child = Object.create(parent);
+// child.childProp = 'this is a child prop';
+
+// console.log(child.hasOwnProperty('a'));
+// console.log(child.a);
+
+
+// const a = {
+//     x: 5,
+// };
+
+// const b = Object.create(a);
+
+// b.y = 10;
+
+// const c = Object.create(b);
+
+// console.log(c);
+// c.hasOwnProperty('x');
+
+
