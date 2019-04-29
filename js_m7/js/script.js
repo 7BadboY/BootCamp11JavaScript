140d1f5e3ee41228f3c2f82b2cf51ab87c5dab5b
Yarotskiy Sergey <abrawa@gmail.com>
2019-04-29 16:30:43 +0300

    module 7 hw done

--- js_m7/js/script.js
+++ js_m7/js/script.js
@@ -169,36 +169,125 @@
 */
 
 
-function createMovieCard() {
-  let card = document.querySelector('.movie');
-  let img = document.createElement('img');
-  img.className = 'movie__image';
-  img.setAttribute('src', 'http://image.tmdb.org/t/p/w500/rPdtLWNsZmAtoZl9PK7S2wE3qiS.jpg');
-  img.setAttribute('alt', 'movie image');
-  card.append(img);
+// function createMovieCard() {
+//   let card = document.querySelector('.movie');
+//   let img = document.createElement('img');
+//   img.className = 'movie__image';
+//   img.setAttribute('src', 'http://image.tmdb.org/t/p/w500/rPdtLWNsZmAtoZl9PK7S2wE3qiS.jpg');
+//   img.setAttribute('alt', 'movie image');
+//   card.append(img);
 
-  let movieBody = document.createElement('div');
-  movieBody.className = 'movie__body';
-  card.append(movieBody);
+//   let movieBody = document.createElement('div');
+//   movieBody.className = 'movie__body';
+//   card.append(movieBody);
 
-  let movieTitle = document.createElement('h2');
-  movieTitle.className = 'movie__title';
-  movieTitle.textContent = 'The Godfather';
-  movieBody.append(movieTitle);
+//   let movieTitle = document.createElement('h2');
+//   movieTitle.className = 'movie__title';
+//   movieTitle.textContent = 'The Godfather';
+//   movieBody.append(movieTitle);
 
-  let movieDiscription = document.createElement('p');
-  movieDiscription.className = 'movie__discription';
-  movieDiscription.textContent = "Spanning the years 1945 to 1955, a chronicle of the fictional Italian-American Corleone crime family. When organized crime family patriarch, Vito Corleone barely survives an attempt on his life, his youngest son, Michael steps in to take care of the would - be killers, launching a campaign of bloody revenge.";
-  movieTitle.append(movieDiscription);
+//   let movieDiscription = document.createElement('p');
+//   movieDiscription.className = 'movie__discription';
+//   movieDiscription.textContent = "Spanning the years 1945 to 1955, a chronicle of the fictional Italian-American Corleone crime family. When organized crime family patriarch, Vito Corleone barely survives an attempt on his life, his youngest son, Michael steps in to take care of the would - be killers, launching a campaign of bloody revenge.";
+//   movieTitle.append(movieDiscription);
 
-  let movieDate = document.createElement('p');
-  movieDate.className = 'movie__date';
-  movieDate.textContent = 'Released: 1972-03-14';
-  movieDiscription.append(movieDate);
+//   let movieDate = document.createElement('p');
+//   movieDate.className = 'movie__date';
+//   movieDate.textContent = 'Released: 1972-03-14';
+//   movieDiscription.append(movieDate);
 
-  let movieRating = document.createElement('p');
-  movieRating.className = 'movie__rating';
-  movieRating.textContent = 'Rating: 8.6';
-  movieDate.append(movieRating);
+//   let movieRating = document.createElement('p');
+//   movieRating.className = 'movie__rating';
+//   movieRating.textContent = 'Rating: 8.6';
+//   movieDate.append(movieRating);
+// }
+// createMovieCard();
+
+// ================================================= task 7 ===========================================
+
+/*
+  В HTML-документе уже есть тег с id="root" (вкладка HTML)
+  
+  Создайте функцию createBoxes(num), которая принимает 1 параметр num - число.
+  
+  Функция создает столько div, сколько указано в num и возвращает их в одном
+  общем контейнере. После чего необходимо повесить результат работы функции
+  в div с id="#root"
+  
+  Каждый div:
+    - Имеет случайный rgb цвет фона
+    - Размеры самого первого div - 30px на 30px.
+    - Каждый следующий div после первого, должен быть шире и выше предыдущего
+      на 10px
+*/
+
+
+// function randomColor () {
+//   let result = '';
+//   let max = 255;
+//   return result = `rgb(${Math.floor(Math.random() * (255 + 1))}, ${Math.floor(Math.random() * (255 + 1))}, ${Math.floor(Math.random() * (255 + 1))})`;
+// } 
+
+// console.log(randomColor());
+
+// function createBoxes(num) {
+//   let mainContainer = document.querySelector('#root');
+//   let container = document.createElement('div');
+//   let i = 1;
+//   let width = 30;
+//   let height = 30;
+
+//   do {
+//     container.innerHTML += `<div style = 'width: ${width + "px"}; height: ${height + "px"}; background-color: ${randomColor()} '></div>`;
+//     i++;
+//     width += 10;
+//     height += 10;
+//   } while (i <= 10);
+//   mainContainer.append(container);
+// }
+// createBoxes(7);
+
+// ================================================================== Home Work 7 module ============================================
+
+/*
+  1. Модифицируйте готовую функцию createPostCard() из задания 
+    номер 6 (https://codepen.io/goit-fe-adv/pen/MVPaeZ) так, 
+    чтобы она принимала объект post с данными для заполнения полей 
+    в карточке.
+      
+  2. Создайте функцию createCards(posts), которая принимает массив
+    объектов-карточек, вызывает функцию createPostCard(post) столько
+    раз, сколько объектов в массиве, сохраняя общий результат и возвращает 
+    массив DOM-элементов всех постов.
+    
+  3. Повесьте все посты в какой-то уже существующий DOM-узел.
+*/
+
+const posts = [
+  {
+    img: "https://placeimg.com/400/150/arch",
+    title: "Post title 1",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+    link: 'link-1.com'
+  },
+  {
+    img: "https://placeimg.com/400/150/nature",
+    title: "Post title 2",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+    link: 'link-2.com'
+  },
+  {
+    img: "https://placeimg.com/400/150/arch",
+    title: "Post title 3",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+    link: 'link-3.com'
+  }
+];
+
+function createPostCard(post) {
+  let mainContainer = document.querySelector('.posts');
+  let list = document.createElement('div');
+  list.innerHTML = post.reduce((acc, el) => acc + `<div><img src="${el.img}"></img><h2>${el.title}</h2><p>${el.text}</p><a href = "${el.link}">${el.link}</a></div>`, '');
+  mainContainer.append(list);
 }
-createMovieCard();
+createPostCard(posts);
